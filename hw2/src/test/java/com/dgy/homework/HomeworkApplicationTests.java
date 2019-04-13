08873334193d35f3bbb384de58eadca3f6e09649
MrHate <dgyhateyou@sjtu.edu.cn>
2019-04-13 11:04:03 +0800

    test for restAPI

--- hw2/src/test/java/com/dgy/homework/HomeworkApplicationTests.java
+++ hw2/src/test/java/com/dgy/homework/HomeworkApplicationTests.java
@@ -1,15 +1,30 @@
 package com.dgy.homework;
 
 import java.util.ArrayList;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import static org.junit.Assert.assertEquals;
+import org.junit.Assert;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HomeworkApplicationTests {
+	private MockMvc mvc;
+
+	@Before
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(new LadderController()).build();
+	}
 
 	@Test
 	public void contextLoads() {
@@ -24,7 +39,17 @@ public class HomeworkApplicationTests {
 		answer.add("cog");
 		answer.add("dog");
 		ArrayList<String> result = Ladder.run("dog","cat");
-		assertEquals(answer,result);
+		Assert.assertEquals(answer,result);
+	}
+
+	@Test
+	public void restTest() throws Exception{
+		RequestBuilder req = null;
+
+		req = MockMvcRequestBuilders.get("/ladder/dog&cat");
+		mvc.perform(req)
+				.andExpect(status().isOk())
+				.andExpect(content().string("[\"cat\",\"cag\",\"cog\",\"dog\"]"));
 	}
 
 	//@Test
