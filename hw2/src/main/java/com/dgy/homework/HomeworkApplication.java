d01efbbf57a7a12c1115b5bd98019532278fbf39
MrHate <dgyhateyou@sjtu.edu.cn>
2019-04-24 13:03:35 +0800

    with new folder for hw3&task3

--- hw2/src/main/java/com/dgy/homework/HomeworkApplication.java
+++ hw2/src/main/java/com/dgy/homework/HomeworkApplication.java
@@ -1,13 +1,24 @@
 package com.dgy.homework;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 public class HomeworkApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomeworkApplication.class, args);
+	}
+
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(
+			@Value("${spring.application.name}") String applicationName) {
+		return (registry) -> registry.config().commonTags("application", applicationName);
 	}
 
 }
