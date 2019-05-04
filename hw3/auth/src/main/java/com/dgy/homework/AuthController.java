582dbc25e1609ba7e51844111dc0f2dbc271ca4f
MrHate <dgyhateyou@sjtu.edu.cn>
2019-05-04 13:19:34 +0800

    skip the bug post not supported

--- hw3/auth/src/main/java/com/dgy/homework/AuthController.java
+++ hw3/auth/src/main/java/com/dgy/homework/AuthController.java
@@ -3,30 +3,25 @@ package com.dgy.homework;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@Controller
-public class ApplicationController {
+@RestController
+public class AuthController {
 	@Autowired
 	private RestTemplate restTemplate;
 
-	@GetMapping("/login")
-	public String login(){
-		return "login.html";
-	}
-
 	@GetMapping("/ladder/{begin}&{end}")
-	public @ResponseBody List<String> generateLadder(@PathVariable String begin, @PathVariable String end){
+	public List<String> generateLadder(@PathVariable String begin, @PathVariable String end){
 		return this.restTemplate.getForObject("http://ladder:8090/ladder/"+begin+"&"+end,List.class);
 	}
 
-	@GetMapping("/success")
-	public @ResponseBody String loginsuccess(){
-		return "success";
+	@GetMapping("/")
+	public String loginsuccess(){
+		return "Login success!\nSend GET request to \"/ladder/{begin}&{end}\" to generate word ladder from ladder microservice.";
 	}
 
 }
--- hw3/auth/src/main/java/com/dgy/homework/SecurityConfig.java
+++ hw3/auth/src/main/java/com/dgy/homework/SecurityConfig.java
@@ -1,31 +1,27 @@
 package com.dgy.homework;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
  
 @Configuration 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	@Autowired
-	private UserDetailsService userDetailsService;
 	 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
-		http.authorizeRequests()
-			.antMatchers("/actuator").hasAuthority("SUPER_ADMIN")
-			.antMatchers("/actuator/**").hasAuthority("SUPER_ADMIN")
-			.antMatchers("/login").permitAll()
-			.anyRequest().authenticated()
-			.and()
+		http.csrf().disable()
+			.authorizeRequests()
+				.antMatchers("/actuator").hasAuthority("SUPER_ADMIN")
+				.antMatchers("/actuator/**").hasAuthority("SUPER_ADMIN")
+				.anyRequest().authenticated()
+				.and()
 			.formLogin()
-			.loginPage("/login").permitAll()
-			.successForwardUrl("/success")
-			.and()
-			.logout().permitAll();
+				.and()
+			.logout()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
 	 
 	}
 }
